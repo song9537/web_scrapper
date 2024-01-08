ec96fc3bada243436dd2d846ce4a077653cfef1a
song9537 <140922961+song9537@users.noreply.github.com>
2024-01-08 15:27:59 -0400

    Completed WWR scrapper

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from requests import get
 from bs4 import BeautifulSoup
 
 base_url = "https://weworkremotely.com/remote-jobs/search?utf8=%E2%9C%93&term="
-search_term = "python"
+search_term = "php"
 
 response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
     print("Can't request website")
 else:
+    
+    results = []
+    
     soup = BeautifulSoup(response.text, "html.parser")
     jobs = soup.find_all('section', class_="jobs")
     for job_section in jobs:
@@ -19,6 +22,15 @@ else:
             link = anchors['href']
             company, kind, region = anchors.find_all('span', class_="company")
             title = anchors.find('span', class_='title')
-            print(company, kind, region, title)
-            print("////////////")
             
+            job_data = {
+                'link': f"https://weworkremotely.com/{link}",
+                'company': company.string,
+                'region': region.string,
+                'position': title.string
+            }
+            
+            results.append(job_data)
+    for result in results:
+        print(result)
+        print("/////////////////")
